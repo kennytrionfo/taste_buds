91e9579c9af17a3a5d4e591c04c3d3e455d7f624
kennytrionfo <kentrionfo@gmail.com>
2015-02-16 08:55:43 -0700

    added associations and updated new and create actions in controller for devise user

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -14,7 +14,7 @@ class ShopsController < ApplicationController
 
   # GET /shops/new
   def new
-    @shop = Shop.new
+    @shop = current_user.shops.build
   end
 
   # GET /shops/1/edit
@@ -24,7 +24,7 @@ class ShopsController < ApplicationController
   # POST /shops
   # POST /shops.json
   def create
-    @shop = Shop.new(shop_params)
+    @shop = current_user.shops.build(shop_params)
 
     respond_to do |format|
       if @shop.save
