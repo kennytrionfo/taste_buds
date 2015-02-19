718c4212fc34f068d85f58eae8ac8901515260a1
kennytrionfo <kentrionfo@gmail.com>
2015-02-18 20:05:06 -0700

    updated controller and index view to show pics and installed imagemagick with homebrew

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -69,6 +69,6 @@ class ShopsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def shop_params
-      params.require(:shop).permit(:title, :description, :flavor, :address, :rating)
+      params.require(:shop).permit(:title, :description, :flavor, :address, :rating, :image)
     end
 end
